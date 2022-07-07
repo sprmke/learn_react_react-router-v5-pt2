3f2b8bd9e83d815872f48205f02a060c9769e436
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-07 13:02:51 +0800

    281. adding dummy data & more content

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,10 @@
-function NewQuote() {
-  return (
-    <div>
-      <h1>New Quote Page</h1>
-    </div>
-  );
-}
+import QuoteForm from '../components/quotes/QuoteForm';
+
+const NewQuote = () => {
+  const addQuoteHandler = (quoteData) => {
+    console.log('quoteData::', quoteData);
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
+};
 
 export default NewQuote;
