15ff47445a868c5e8480fabaf58018e5611265b6
Michael Manlulu <michael.manlulu@myresman.com>
2022-07-08 10:51:33 +0800

    290. adding 'comments' features

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,66 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+
+import NewCommentForm from './NewCommentForm';
+import CommentsList from './CommentsList';
+import LoadingSpinner from '../ui/LoadingSpinner';
 
 import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
 
 const Comments = () => {
+  const params = useParams();
+  const { quoteId } = params;
+
   const [isAddingComment, setIsAddingComment] = useState(false);
+
+  const {
+    sendRequest,
+    data: comments,
+    status,
+    error,
+  } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
+
+  // Comments Content
+  let commentsContent = null;
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (status === 'completed' && error) {
+    commentsContent = (
+      <div className='center'>
+        <p>{error}</p>
+      </div>
+    );
+  }
+  if (status === 'completed' && !error && comments.length === 0) {
+    commentsContent = (
+      <div className='center'>
+        <p>No comments added yet!</p>
+      </div>
+    );
+  }
+  if (status === 'completed' && !error && comments.length > 0) {
+    commentsContent = <CommentsList comments={comments} />;
+  }
 
   return (
     <section className={classes.comments}>
@@ -18,8 +70,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={params.quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      {commentsContent}
     </section>
   );
 };
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,26 +1,50 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
+
+import LoadingSpinner from '../../components/ui/LoadingSpinner';
 
 import classes from './NewCommentForm.module.css';
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
 
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === 'completed' && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
+
   const submitFormHandler = (event) => {
     event.preventDefault();
 
+    const text = commentTextRef.current.value;
     // optional: Could validate here
 
     // send comment to server
+    sendRequest({ quoteId: props.quoteId, commentData: { text } });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
-      <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
-      </div>
-      <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+      {status === 'pending' && (
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>
+      )}
+      <div>
+        <div className={classes.control} onSubmit={submitFormHandler}>
+          <label htmlFor='comment'>Your Comment</label>
+          <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button className='btn'>Add Comment</button>
+        </div>
       </div>
     </form>
   );
